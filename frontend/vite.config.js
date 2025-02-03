92d0c79a275fa407184aee2cc6d277d1bbcc6f66
Dushyanth Yalamanchili <167439018+dushu-108@users.noreply.github.com>
2025-02-04 02:29:31 +0530

    go

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	root: resolve(__dirname),
-	base: '/',
-	build: {
-		outDir: resolve(__dirname, '../public'),
-		emptyOutDir: true
-	},
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:4000",
-			},
-		},
-	}
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: '../public',
+    emptyOutDir: true,
+  },
 });
